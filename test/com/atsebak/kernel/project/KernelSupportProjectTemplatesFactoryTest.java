59cc04a250d437b82fa00c1c6f59d749e13a56bc
ahmadsebak <radio_frequency@hotmail.com>
2015-05-17 16:41:31 -0400

    added test directory

---
+++ test/com/atsebak/kernel/project/KernelSupportProjectTemplatesFactoryTest.java
@@ -0,0 +1,17 @@
+package com.atsebak.kernel.project;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class KernelSupportProjectTemplatesFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
